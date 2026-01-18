ae85a4083e25680965a88a9b2b6d10f397fa76b8
Anugrahaa07 <anugraha.a2020@gmail.com>
2026-01-19 03:57:42 +0530

    bool

---
+++ bool.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<stdbool.h>
+void main()
+{
+    bool isstudent = true;
+    
+    if(isstudent){
+        printf("You are a student");
+    }
+
+    else{
+        printf("You are NOT a student");
+    }
+}
